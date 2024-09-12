a4c9317f714b2d6ccd5aab7313122202218aecc8
Yogesh Palve <100296461+YogeshDPalve@users.noreply.github.com>
2024-09-12 23:31:16 +0530

    minor changes

--- src/controllers/healthCheck_controller.js
+++ src/controllers/healthCheck_controller.js
@@ -4,4 +4,3 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 export const healthcheck = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponce(200, "OK", "Heath check passed"));
 });
-
